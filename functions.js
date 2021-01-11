08546c76440e09040d737d08985679d0ce5f3df0
mhussein15 <mhalfakih15@gmail.com>
2021-01-11 10:04:30 +0300

    Task Done

--- functions.js
+++ functions.js
@@ -7,7 +7,7 @@
  * greet("Hamza") logs "Hello Hamza"
  */
 function greet(name) {
-  // Your code here
+  console.log(`Hello ${name}`);
 }
 
 /**
@@ -20,7 +20,11 @@ function greet(name) {
  * isOdd(10) -> false
  */
 function isOdd(n) {
-  // Your code here
+  if (n % 2 === 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 /**
@@ -33,7 +37,7 @@ function isOdd(n) {
  * oddsSmallerThan(15) -> 7
  */
 function oddsSmallerThan(n) {
-  // Your code here
+  return Math.floor(n / 2);
 }
 
 /**
@@ -47,7 +51,11 @@ function oddsSmallerThan(n) {
  * squareOrDouble(9) -> 81
  */
 function squareOrDouble(n) {
-  // Your code here
+  if (n % 2 === 0) {
+    return n * 2;
+  } else {
+    return n * n;
+  }
 }
 
 module.exports = { greet, isOdd, oddsSmallerThan, squareOrDouble };
